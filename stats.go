30d5d81b56c6fffd2a473bdb9e5d61965adf3bd2
krisjczhang <krisjczhang@tencent.com>
2022-09-27 20:13:27 +0800

    优化mtr，自动判断是否完成

--- stats.go
+++ stats.go
@@ -137,7 +137,7 @@ func (t *TraceRoute) IsFinish() bool {
 					return true
 				}
 			} else {
-				if cur.Sub(t.StartTime).Seconds() > 20 {
+				if cur.Sub(t.StartTime).Seconds() > 40 {
 					return true
 				}
 			}
