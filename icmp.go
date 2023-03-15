2a08010fe0fbe4e5447b04ca42ddf6f9908e8577
krisjczhang <krisjczhang@tencent.com>
2023-03-15 15:36:06 +0800

    增加模式1

--- icmp.go
+++ icmp.go
@@ -31,10 +31,6 @@ func (t *TraceRoute) SendIPv4ICMP() error {
 	if err != nil {
 		return err
 	}
-	err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true)
-	if err != nil {
-		return fmt.Errorf("SetControlMessage()，%s", err)
-	}
 	ipaddr, err := net.ResolveIPAddr("ip4", t.NetDstAddr.String())
 	if err != nil {
 		return err
@@ -69,14 +65,17 @@ func (t *TraceRoute) SendIPv4ICMP() error {
 			if err != nil {
 				return err
 			}
+			err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true)
+			if err != nil {
+				return fmt.Errorf("SetControlMessage()，%s", err)
+			}
 			if err = conn.IPv4PacketConn().SetTTL(ttl); err != nil {
 				return fmt.Errorf("conn.IPv4PacketConn().SetTTL()失败，%s", err)
 			}
-			s, err := conn.WriteTo(msgBytes, addr)
+			_, err = conn.WriteTo(msgBytes, addr)
 			if err != nil {
 				return fmt.Errorf("conn.WriteTo()失败，%s", err)
 			}
-			fmt.Println(fmt.Sprintf("WriteTo：%d", s))
 			m := &SendMetric{
 				FlowKey:   key,
 				ID:        uint32(id),
@@ -106,13 +105,12 @@ func (t *TraceRoute) ListenIPv4ICMP() error {
 	for {
 		// 包+头
 		buf := make([]byte, 1500)
-		if err := conn.SetReadDeadline(time.Now().Add(time.Millisecond * 500)); err != nil {
+		if err := conn.SetReadDeadline(time.Now().Add(time.Millisecond * 1500)); err != nil {
 			return err
 		}
 		// tmd，在苹果手机(底层是ios)上这个ReadFrom会阻塞读，在ios模拟器(底层是dawrin)上就没事
 		// md，怎么在android又是另一个情况，不仅阻塞住了，而且一直读不到东西
 		//n, _, src, err := conn.IPv4PacketConn().ReadFrom(buf)
-		fmt.Println(fmt.Sprintf("是否建立conn：%v", conn == nil))
 		n, src, err := conn.ReadFrom(buf)
 		if src != nil {
 			fmt.Println(src.String())
