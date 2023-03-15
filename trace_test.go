bb515c62eb429483c43fe9ff8d2e3a0aa9dd9c0e
krisjczhang <krisjczhang@tencent.com>
2023-03-15 15:36:36 +0800

    增加模式1

--- trace_test.go
+++ trace_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestTrace(t *testing.T) {
-	c, err := New("icmp", "8.8.8.8", "", "ip4", 3, 6, "icmp")
+	c, err := New("icmp", "8.8.8.8", "", "ip4", 3, 6, "udp")
 	if err != nil {
 		t.Fatal(err)
 		return
