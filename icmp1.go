e52ec0c30df2663bea6d3416192945d2a38902fd
krisjczhang <krisjczhang@tencent.com>
2023-03-17 20:27:26 +0800

    增加模式2

--- icmp1.go
+++ icmp1.go
@@ -14,14 +14,13 @@ import (
 	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
 	"net"
+	"os"
 	"runtime"
+	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
-
-//func (t *TraceRoute) run() error {
-//
-//}
 
 func (t *TraceRoute) SendIPv4ICMP1() error {
 	ip, err := getClientIp()
@@ -30,7 +29,7 @@ func (t *TraceRoute) SendIPv4ICMP1() error {
 	}
 	fmt.Println(ip)
 	fmt.Println(t.NetSrcAddr.String())
-	conn, err := icmp.ListenPacket(ipv4Proto[t.PingType], ip)
+	conn, err := ListenPacket(ipv4Proto[t.PingType], ip)
 	if err != nil {
 		return err
 	}
@@ -58,15 +57,15 @@ func (t *TraceRoute) SendIPv4ICMP1() error {
 	if err != nil {
 		return err
 	}
-	err = conn.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true)
+	err = conn.SetControlMessage(ipv4.FlagTTL, true)
 	if err != nil {
 		return fmt.Errorf("SetControlMessage()，%s", err)
 	}
 	fmt.Println(fmt.Sprintf("TTL：%d", t.Count))
-	if err = conn.IPv4PacketConn().SetTTL(t.Count); err != nil {
+	if err = conn.SetTTL(t.Count); err != nil {
 		return fmt.Errorf("conn.IPv4PacketConn().SetTTL()失败，%s", err)
 	}
-	_, err = conn.WriteTo(msgBytes, addr)
+	_, err = conn.WriteTo(msgBytes, nil, addr)
 	if err != nil {
 		return fmt.Errorf("conn.WriteTo()失败，%s", err)
 	}
@@ -81,7 +80,7 @@ func (t *TraceRoute) SendIPv4ICMP1() error {
 			return err
 		}
 		fmt.Println(runtime.GOOS)
-		n, src, err := conn.ReadFrom(buf)
+		n, _, src, err := conn.ReadFrom(buf)
 		if n > 0 {
 			fmt.Println(n)
 			fmt.Println(src.String())
@@ -184,4 +183,118 @@ func getClientIp() (string, error) {
 
 	return "", errors.New("Can not find the client ip address!")
 
+}
+
+func ListenPacket(network, address string) (*ipv4.PacketConn, error) {
+	var family, proto int
+	switch network {
+	case "udp4":
+		family, proto = syscall.AF_INET, protocolICMP
+	case "udp6":
+		family, proto = syscall.AF_INET6, protocolIPv6ICMP
+	default:
+		i := last(network, ':')
+		if i < 0 {
+			i = len(network)
+		}
+		switch network[:i] {
+		case "ip4":
+			proto = protocolICMP
+		case "ip6":
+			proto = protocolIPv6ICMP
+		}
+	}
+	var cerr error
+	var c net.PacketConn
+	switch family {
+	case syscall.AF_INET, syscall.AF_INET6:
+		s, err := syscall.Socket(family, syscall.SOCK_DGRAM, proto)
+		if err != nil {
+			return nil, os.NewSyscallError("socket", err)
+		}
+		if err := syscall.SetsockoptInt(s, 0, 0x17, 1); err != nil {
+			syscall.Close(s)
+			return nil, os.NewSyscallError("setsockopt", err)
+		}
+		sa, err := sockaddr(family, address)
+		if err != nil {
+			syscall.Close(s)
+			return nil, err
+		}
+		if err := syscall.Bind(s, sa); err != nil {
+			syscall.Close(s)
+			return nil, os.NewSyscallError("bind", err)
+		}
+		f := os.NewFile(uintptr(s), "datagram-oriented icmp")
+		c, cerr = net.FilePacketConn(f)
+		f.Close()
+	default:
+		c, cerr = net.ListenPacket(network, address)
+	}
+	if cerr != nil {
+		return nil, cerr
+	}
+	return ipv4.NewPacketConn(c), nil
+}
+
+func last(s string, b byte) int {
+	i := len(s)
+	for i--; i >= 0; i-- {
+		if s[i] == b {
+			break
+		}
+	}
+	return i
+}
+
+func sockaddr(family int, address string) (syscall.Sockaddr, error) {
+	switch family {
+	case syscall.AF_INET:
+		a, err := net.ResolveIPAddr("ip4", address)
+		if err != nil {
+			return nil, err
+		}
+		if len(a.IP) == 0 {
+			a.IP = net.IPv4zero
+		}
+		if a.IP = a.IP.To4(); a.IP == nil {
+			return nil, net.InvalidAddrError("non-ipv4 address")
+		}
+		sa := &syscall.SockaddrInet4{}
+		copy(sa.Addr[:], a.IP)
+		return sa, nil
+	case syscall.AF_INET6:
+		a, err := net.ResolveIPAddr("ip6", address)
+		if err != nil {
+			return nil, err
+		}
+		if len(a.IP) == 0 {
+			a.IP = net.IPv6unspecified
+		}
+		if a.IP.Equal(net.IPv4zero) {
+			a.IP = net.IPv6unspecified
+		}
+		if a.IP = a.IP.To16(); a.IP == nil || a.IP.To4() != nil {
+			return nil, net.InvalidAddrError("non-ipv6 address")
+		}
+		sa := &syscall.SockaddrInet6{ZoneId: zoneToUint32(a.Zone)}
+		copy(sa.Addr[:], a.IP)
+		return sa, nil
+	default:
+		return nil, net.InvalidAddrError("unexpected family")
+	}
+}
+
+func zoneToUint32(zone string) uint32 {
+	if zone == "" {
+		return 0
+	}
+	if ifi, err := net.InterfaceByName(zone); err == nil {
+		return uint32(ifi.Index)
+	}
+	n, err := strconv.Atoi(zone)
+	if err != nil {
+		return 0
+	}
+	return uint32(n)
 }
