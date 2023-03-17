ea3a64464f12b58e965fc8c2e15f57e25cafdb0e
krisjczhang <krisjczhang@tencent.com>
2023-03-17 10:41:40 +0800

    增加模式2

--- icmp1.go
+++ icmp1.go
@@ -23,7 +23,7 @@ import (
 //}
 
 func (t *TraceRoute) SendIPv4ICMP1() error {
-	conn, err := icmp.ListenPacket(ipv4Proto[t.PingType], "0.0.0.0")
+	conn, err := icmp.ListenPacket(ipv4Proto[t.PingType], "127.0.0.1")
 	if err != nil {
 		return err
 	}
