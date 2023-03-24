dbd6df2355e0236f71b06e8fdd50a1c66cd393d5
krisjczhang <krisjczhang@tencent.com>
2023-03-24 18:04:04 +0800

    增加android执行方式

--- android.go
+++ android.go
@@ -8,44 +8,77 @@ package ztrace
 
 import (
 	"fmt"
-	"net"
 	"os/exec"
 	"strings"
 	"time"
 )
 
 func (t *TraceRoute) ExecCmd() error {
-	ips, err := net.LookupHost("www.qq.com")
-	if err != nil {
-		return err
-	}
-	if len(ips) == 0 {
-		return fmt.Errorf("目的地址未能解析出IP")
-	}
-	//target := ips[0]
-	for i := 2; i <= 64; i++ {
-		ttl := fmt.Sprintf("-t %d", i)
-		fmt.Println(ttl)
-		cmd := NewExecute()
-		stdOut, _, err := cmd.RunWithTimeout(time.Millisecond*200, "ping", "-i 0.2", "-c 1", ttl, "-W 200", "www.qq.com")
-		if _, ok := err.(*exec.ExitError); ok {
+	key := GetHash(t.NetSrcAddr.To4(), t.NetDstAddr.To4(), 65535, 65535, 1)
+	db := NewStatsDB(key)
+	t.DB.Store(key, db)
+	go db.Cache.Run()
 
+	//ips, err := net.LookupHost("www.qq.com")
+	//if err != nil {
+	//	return err
+	//}
+	//if len(ips) == 0 {
+	//	return fmt.Errorf("目的地址未能解析出IP")
+	//}
+	//target := ips[0]
+	lastHop := 64
+	for c := 1; c <= t.Count; c++ {
+		for i := 1; i <= lastHop; i++ {
+			ttl := fmt.Sprintf("-t %d", i)
+			cmd := NewExecute()
+			m := &SendMetric{
+				FlowKey:   key,
+				ID:        uint32(i),
+				TTL:       uint8(i),
+				TimeStamp: time.Now(),
+			}
+			t.RecordSend(m)
+			stdOut, _, err := cmd.RunWithTimeout(time.Millisecond*200, "ping", "-i 0.2", "-c 1", ttl, "-W 200", "8.8.8.8")
+			if _, ok := err.(*exec.ExitError); ok {
+
+			}
+			hopIp := t.parseHopIp(stdOut, t.Count, i)
+			fmt.Println(fmt.Sprintf("%d --- %s", i, hopIp))
+			if hopIp == t.NetDstAddr.String() {
+				t.Statistics()
+				fmt.Println(t.HopStr)
+				break
+			}
 		}
-		hopIp := parseHopIp(stdOut)
-		fmt.Println(hopIp)
 	}
+
 	return nil
 }
 
-func parseHopIp(text string) string {
+func (t *TraceRoute) parseHopIp(text string, count, ttl int) string {
+	key := GetHash(t.NetSrcAddr.To4(), t.NetDstAddr.To4(), 65535, 65535, 1)
 	var hopIp string
 	arr := strings.Split(text, "\n")
-	str := arr[1]
-	strArr := strings.Fields(str)
-	if strings.Contains(str, "ttl") {
-		hopIp = strArr[3]
-	} else {
-		hopIp = strArr[1]
+	if len(arr) < count+1 {
+		return "*"
 	}
+	// 从第二行到第count行
+	for i := 1; i <= count; i++ {
+		row := arr[i]
+		word := strings.Fields(row)
+		if strings.Contains(row, "ttl") {
+			hopIp = word[3]
+		} else {
+			hopIp = word[1]
+		}
+	}
+	recv := &RecvMetric{
+		FlowKey:   key,
+		ID:        uint32(ttl),
+		RespAddr:  hopIp,
+		TimeStamp: time.Now(),
+	}
+	t.RecordRecv(recv)
 	return hopIp
 }
