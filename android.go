6528cf427d611580dc7c0cfc1a1dda3ddc88fe7f
krisjczhang <krisjczhang@tencent.com>
2023-03-24 18:44:32 +0800

    增加android执行方式

--- android.go
+++ android.go
@@ -19,14 +19,6 @@ func (t *TraceRoute) ExecCmd() error {
 	t.DB.Store(key, db)
 	go db.Cache.Run()
 
-	//ips, err := net.LookupHost("www.qq.com")
-	//if err != nil {
-	//	return err
-	//}
-	//if len(ips) == 0 {
-	//	return fmt.Errorf("目的地址未能解析出IP")
-	//}
-	//target := ips[0]
 	lastHop := 64
 	for c := 1; c <= t.Count; c++ {
 		for i := 1; i <= lastHop; i++ {
@@ -46,9 +38,10 @@ func (t *TraceRoute) ExecCmd() error {
 			hopIp := t.parseHopIp(stdOut, t.Count, i)
 			fmt.Println(fmt.Sprintf("%d --- %s", i, hopIp))
 			if hopIp == t.NetDstAddr.String() {
+				// 减少循环次数
 				lastHop = i
+				// 设置最后一跳
 				t.LastHop = i
-				break
 			}
 		}
 	}
@@ -69,6 +62,7 @@ func (t *TraceRoute) parseHopIp(text string, count, ttl int) string {
 		row := arr[i]
 		word := strings.Fields(row)
 		if strings.Contains(row, "ttl") {
+			// 8.8.8.8:
 			hopIp = strings.ReplaceAll(word[3], ":", "")
 		} else {
 			hopIp = word[1]
