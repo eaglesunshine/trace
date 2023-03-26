5f41b5809fd63fd17fab4e6c9b0559309c569c7c
krisjczhang <krisjczhang@tencent.com>
2023-03-26 16:03:55 +0800

    增加android执行方式

--- android.go
+++ android.go
@@ -20,55 +20,46 @@ func (t *TraceRoute) ExecCmd() error {
 	go db.Cache.Run()
 
 	lastHop := 64
-	for c := 1; c <= t.Count; c++ {
-		for i := 1; i <= lastHop; i++ {
-			ttl := fmt.Sprintf("-t %d", i)
-			cmd := NewExecute()
-			m := &SendMetric{
-				FlowKey:   key,
-				ID:        uint32(i),
-				TTL:       uint8(i),
-				TimeStamp: time.Now(),
-			}
-			t.RecordSend(m)
-			stdOut, _, err := cmd.RunWithTimeout(time.Millisecond*200, "/system/bin/ping",
-				"-i 0.2", "-c 1", ttl, "-W 200", t.Dest)
-			if _, ok := err.(*exec.ExitError); ok {
-				fmt.Println(err.Error())
-			}
-			fmt.Println(stdOut)
-			hopIp := t.parseHopIp(stdOut, t.Count, i)
-			fmt.Println(fmt.Sprintf("%d --- %s", i, hopIp))
-			if hopIp == t.NetDstAddr.String() {
-				// 减少循环次数
-				lastHop = i
-				// 设置最后一跳
-				t.LastHop = i
-			}
+	for i := 1; i <= lastHop; i++ {
+		ttl := fmt.Sprintf("-t %d", i)
+		cmd := NewExecute()
+		m := &SendMetric{
+			FlowKey:   key,
+			ID:        uint32(i),
+			TTL:       uint8(i),
+			TimeStamp: time.Now(),
+		}
+		t.RecordSend(m)
+		timeout := time.Millisecond * 200
+		stdOut, _, err := cmd.RunWithTimeout(timeout, "/system/bin/ping", "-c 1", ttl, "-W 200", t.Dest)
+		if _, ok := err.(*exec.ExitError); ok {
+			fmt.Println(err.Error())
+		}
+		hopIp := t.parseHopIp(stdOut, i)
+		if hopIp == t.NetDstAddr.String() {
+			// 设置最后一跳
+			t.LastHop = i
+			break
 		}
 	}
 	t.Statistics()
-	fmt.Println(t.HopStr)
 	return nil
 }
 
-func (t *TraceRoute) parseHopIp(text string, count, ttl int) string {
+func (t *TraceRoute) parseHopIp(text string, ttl int) string {
 	key := GetHash(t.NetSrcAddr.To4(), t.NetDstAddr.To4(), 65535, 65535, 1)
 	var hopIp string
 	arr := strings.Split(text, "\n")
 	if len(arr) == 1 {
 		return "*"
 	}
-	// 从第二行到第count行
-	for i := 1; i <= count; i++ {
-		row := arr[i]
-		word := strings.Fields(row)
-		if strings.Contains(row, "ttl") {
-			// 8.8.8.8:
-			hopIp = strings.ReplaceAll(word[3], ":", "")
-		} else {
-			hopIp = word[1]
-		}
+	row := arr[1]
+	word := strings.Fields(row)
+	if strings.Contains(row, "ttl") {
+		// 8.8.8.8:
+		hopIp = strings.ReplaceAll(word[3], ":", "")
+	} else {
+		hopIp = word[1]
 	}
 	recv := &RecvMetric{
 		FlowKey:   key,
