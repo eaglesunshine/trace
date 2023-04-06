46830562becb8ea3f2202305de27db173c012440
krisjczhang <krisjczhang@tencent.com>
2023-04-06 21:25:59 +0800

    fix

--- android.go
+++ android.go
@@ -33,7 +33,7 @@ func (t *TraceRoute) ExecCmd() error {
 			}
 			t.RecordSend(m)
 			timeout := time.Millisecond * 200
-			stdOut, _, err := cmd.RunWithTimeout(timeout, "ping", "-c 1", ttl, "-W 200", t.Dest)
+			stdOut, _, err := cmd.RunWithTimeout(timeout, "/system/bin/ping", "-c 1", ttl, "-W 200", t.Dest)
 			if _, ok := err.(*exec.ExitError); ok {
 			}
 			hopIp := t.parseHopIp(stdOut, i)
